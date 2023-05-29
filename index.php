7e5ea80564624d982aecaa1870ffbe2023e38868
Fabio-Tarozzi <118119098+Fabio-Tarozzi@users.noreply.github.com>
2023-05-29 11:18:33 +0200

    Update index.php
    solved problem

--- index.php
+++ index.php
@@ -3,31 +3,38 @@
 <head>
     <title>Rilevazione temperatura</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .lala{
+            decoration: none;
+            border: none;
+            font-size: 30px;
+            width: 60px;
+        }
+    </style>
 </head>
 <body>
     <header>
         <h1 class="ciao">TEMPERATURA RILEVATA</h1>
     </header>
 	<div class="container">
-    <form method="post">
+    <form method="post" enctype="multipart/form-data">
 		<div class="aaa">
 			<h2>Inserire nome sostanza:</h2>
 			<input id="sostanza" type="text" name="sostanza">
 		</div>
 		<div class="random-number">
-            <input type="hidden" id="inputValore" name="inputValore"/>
+            <input class="lala" type="text" id="inputValore" name="inputValore" value="">
 			<script>
-                var valore = document.getElementById("inputValore").value;
 				var min = -5;
 				var max = 25;
-				valore = Math.floor(Math.random() * (max - min + 1)) + min;
-				document.write(valore + " °C");
+				var valore = Math.floor(Math.random() * (max - min + 1)) + min;
+				document.getElementById("inputValore").value = valore + " °C";
 			</script>
 		</div>
-    </form>
 	</div>
-    <form method="post">
-	    <div class="button" name="invia"><button>INVIA</button></div>
+	    <div class="button">
+            <button id="invia" name="invia">INVIA</button>
+        </div>
     </form>
 </body>
 </html>
@@ -37,24 +44,22 @@
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "ArpaDb";
+        $dbname = "arpa";
+     
+        $temp = $_REQUEST["inputValore"];
+        $sostanza = $_REQUEST["sostanza"];
 
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $temp = $_POST["inputValore"];;
-            $data = date('Y-m-d');
-            $sostanza = $_POST["sostanza"];
 
             $sql = "INSERT INTO rilevazione (temp, sostanza) VALUES (:temp, :sostanza)";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':temp', $temp);
-            //$stmt->bindParam(':data', $data);
             $stmt->bindParam(':sostanza', $sostanza);
             $stmt->execute();
 
-            echo "Valori inseriti correttamente nel database";
         } catch(PDOException $e) {
             echo "Errore nell'inserimento dei valori nel database: " . $e->getMessage();
         }
